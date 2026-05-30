a0e6d51cca9138cb1a2fae39a59b82c418a6fc92
Michel <josemichel18br@gmail.com>
2026-05-29 22:59:45 -0300

    docs: add javadoc to Priority and Status enums

--- src/main/java/com/logistics/taskflow/entity/Priority.java
+++ src/main/java/com/logistics/taskflow/entity/Priority.java
@@ -1,5 +1,13 @@
 package com.logistics.taskflow.entity;
 
+/**
+ * Enum representing the urgency level of a task.
+ * <ul>
+ *   <li>{@code LOW} - Non-critical task, can be addressed later.</li>
+ *   <li>{@code MEDIUM} - Standard priority task. Default value.</li>
+ *   <li>{@code HIGH} - Urgent task, must be addressed immediately.</li>
+ * </ul>
+ */
 public enum Priority {
     LOW,
     MEDIUM,
--- src/main/java/com/logistics/taskflow/entity/Status.java
+++ src/main/java/com/logistics/taskflow/entity/Status.java
@@ -1,5 +1,13 @@
 package com.logistics.taskflow.entity;
 
+/**
+ * Enum representing the lifecycle status of a task.
+ * <ul>
+ *   <li>{@code TODO} - Task has not been started yet.</li>
+ *   <li>{@code IN_PROGRESS} - Task is currently being worked on.</li>
+ *   <li>{@code DONE} - Task has been completed.</li>
+ * </ul>
+ */
 public enum Status {
     TODO,
     IN_PROGRESS,
